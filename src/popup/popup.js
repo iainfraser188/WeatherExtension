f64c03906bb52d1fed08b96fe794b9ab16f1150b
OPTI-IFraser <ifraser@optiseller.com>
2025-06-13 11:49:47 +0100

    added popup.js and .html code

---
+++ src/popup/popup.js
@@ -0,0 +1,8 @@
+chrome.storage.local.get('weatherData', (result) => {
+    const data = result.weatherData;
+    const weatherDiv = document.getElementById('weather');
+    if (data) {
+        weatherDiv.innerHTML = 
+        <p>${data.location.name}</p>
+    }
+});
